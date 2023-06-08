0aa70c50cddecabce7e5f6c0e91b7166a4c65cf3
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-06-08 15:34:56 -0300

    Add/Controle Noticias

--- src/main/controle/ControleNoticias.js
+++ src/main/controle/ControleNoticias.js
@@ -5,17 +5,75 @@ const ServicoNoticias = require("../servico/ServicoNoticias");
 
 module.exports = class ControleNoticias {
 
-  static async listarTodas(req, res) {   
+  static async criarNoticia(req, res) {
     try {
-        res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-        res.header('X-Total-Count', await ServicoNoticias.getCountDocuments());
-        res.status(200).send(await ServicoNoticias.listarTodas(req.query));        
-    } catch (e) {
-        res.status(500).send(e.message);
-        global.logger.error("ControleNoticias.listarTodas " + e.message);
+      const noticia = req.body;
+      console.log(req.body)
+      const novoNoticia = await ServicoNoticias.criarNoticia(noticia);
+      res.status(201).json(novoNoticia);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } // findAll()
-  
- 
-  
+  }// create
+
+  static async buscarNoticias(req, res) {
+    try {
+      const noticias = await ServicoNoticias.buscarNoticias();
+      res.json(noticias);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }// findAll
+
+  static async buscarNoticiaPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const noticia = await ServicoNoticias.buscarNoticiaPorId(id);
+      res.json(noticia);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }// findByID
+
+  static async buscarNoticiaPorTitulo(req, res) {
+    try {
+      const titulo = req.params.titulo;
+      const noticia = await ServicoNoticias.buscarNoticiaPorTitulo(titulo);
+      res.json(noticia);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }// findByName
+
+  static async buscarNoticiasPorData(req, res) {
+    try {
+      const data = req.params.data;
+      const noticias = await ServicoNoticias.buscarNoticiasPorData(data);
+      res.json(noticias);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }// findByDate
+
+  static async atualizarNoticia(req, res) {
+    try {
+      const id = req.params.id;
+      const noticiaAtualizado = req.body;
+      const noticia = await ServicoNoticias.atualizarNoticia(id, noticiaAtualizado);
+      res.json(noticia);
+    } catch (error) {
+      res.status(500).json({error: error.message})
+    }
+  }// update
+
+  static async excluirNoticia(req, res) {
+    try {
+      const id = req.params.id;
+      const noticiaExcluido = await ServicoNoticias.excluirNoticia(id);
+      res.json({ sucesso: noticiaExcluido });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
 }; // class
