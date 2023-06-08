5e55859ad23f0f94a92d9ab5a0686603a71b58f8
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-06-08 15:33:58 -0300

    Add/Rota Noticias

--- src/main/rotas/RotaNoticias.js
+++ src/main/rotas/RotaNoticias.js
@@ -6,8 +6,15 @@ const ControleNoticias = require("../controle/ControleNoticias");
 module.exports = class RotaNoticias {
     constructor(app) {
         app.route("/noticias")
-            .get(ControleNoticias.listarTodas)
-           
+            .post(ControleNoticias.criarNoticia)
+            .get(ControleNoticias.buscarNoticias)
+        app.route("/noticias/:id")
+            .put(ControleNoticias.atualizarNoticia)
+            .delete(ControleNoticias.excluirNoticia)
+            .get(ControleNoticias.buscarNoticiaPorId)
+        app.route("/noticias/titulo/:titulo")
+            .get(ControleNoticias.buscarNoticiaPorTitulo)
+        app.route("/noticias/data/:data")
+            .get(ControleNoticias.buscarNoticiasPorData)
     } // constructor()
-
 } // class
